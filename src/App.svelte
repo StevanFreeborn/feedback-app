9e941f4750f0b5eed461aaa68b9f55ca7d839f4e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 20:43:39 -0500

    feat: add feedback list and item component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-  </script>
+  import FeedbackList from "./components/FeedbackList.svelte";
+
+  let feedback = [
+    { id: 1, rating: 10, text: "This is the first feedback" },
+    { id: 2, rating: 5, text: "This is the second feedback" },
+    { id: 3, rating: 8, text: "This is the third feedback" },
+  ];
+
+</script>
 
 <main>
-  <h1>Hello World</h1>
+  <FeedbackList {feedback} />
 </main>
 
 <style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  type Feedback = {
+    id: number;
+    rating: number;
+    text: string;
+  };
+
+  export let feedback: Feedback[] = [];
+</script>
+
+{#each feedback as { id, rating, text }}
+  <div class="feedback">
+    <h2>Feedback {id}</h2>
+    <p>Rating: {rating}</p>
+    <p>{text}</p>
+  </div>
+{/each}
