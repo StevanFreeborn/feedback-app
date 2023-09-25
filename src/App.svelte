a690b302672cba94876bec34f4cf8eec9972162e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-09-24 21:07:29 -0500

    feat: add reactive count and average

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,9 @@
     { id: 3, rating: 8, text: "This is the third feedback" },
   ];
 
+  $: count = feedback.length;
+  $: average = feedback.reduce((acc, curr) => acc + curr.rating, 0) / feedback.length;
+
   function handleDeleteFeedback (e: CustomEvent<number>) {
     const id = e.detail;
     feedback = feedback.filter((fb) => fb.id !== id);
